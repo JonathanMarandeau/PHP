f00e6ec76a3b02055a238f4d64fde01efb943898
JonathanMarandeau <jonathan.marandeau@gmail.com>
2018-05-14 14:21:56 +0200

    Création d'une variable km

---
+++ www/PhpPartie1/exercice3/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie 1 Exercice 3</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1>Créer une variable km. L'initialiser à 1. Afficher son contenu.
+        Changer sa valeur par 3. Afficher son contenu.
+        Changer sa valeur par 125. Afficher son contenu.
+    </h1>
+    <p>
+      <?php
+        $km = 1;
+        echo $km;
+      ?></br>
+      <?php
+        $km = 3;
+        echo $km
+      ?></br>
+      <?php
+        $km = 125;
+        echo $km
+        ?>
+   </p>
+  </body>
+</html>
