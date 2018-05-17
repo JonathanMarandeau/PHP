1f75aa33981557b8c89be626ee51606a86e92081
JonathanMarandeau <jonathan.marandeau@gmail.com>
2018-05-17 09:18:31 +0200

    Fonction avec parametre string et qui retourne string

---
+++ php4/exercice2/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie 4 Exercice 2</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <h1>PHP Partie 4 Exercice 2</h1>
+      <div class="order">
+        <p>Faire une fonction qui prend en paramètre une chaine de caractères et qui retourne cette même chaine.</p>
+      </div>
+    <h2>exercice 2</h2>
+    <div class="content">
+      <p>
+        <?php
+        //Je crée une variable de chaine de caractère
+        $string = 'Booonnnnjouuuuurrr Mickaeeeeeeelllll';
+        //Je crée ma fonction returnString avec comme paramètre ma variable $string
+        function returnString($string){
+        //Je demande d'afficher ma variable
+        echo $string;
+        }
+        //Je fais appel à ma fonction
+        returnString($string);
+        ?>
+      </p>
+    </div>
+  </body>
+</html>
