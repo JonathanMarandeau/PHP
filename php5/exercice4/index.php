d7fc84c14e241f827882d92889494092d6973893
JonathanMarandeau <jonathan.marandeau@gmail.com>
2018-05-18 09:54:57 +0200

    Modification de aout en août

---
+++ php5/exercice4/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie 5 Exercice 4</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <h1>PHP Partie 5 Exercice 4</h1>
+      <div class="order">
+        <p>Avec le tableau de l'exercice 1, modifier le mois de aout pour lui ajouter l'accent manquant.</p>
+      </div>
+    <h2>Exercice 4</h2>
+      <div class="content">
+        <!--Je crée un tableau mois-->
+        <p><?php $months = array ('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'); ?></p>
+        <!--Je récupère la ligne où il y'a aout pour la modifier en août-->
+        <p><?php $months[7] = 'août'; ?></p>
+        <!--J'affiche le tableau avec la boucle foreach-->
+        <p><?php foreach ($months as $month) {
+          echo $month . ' ';
+        }?></p>
+      </div>
+  </body>
+</html>
