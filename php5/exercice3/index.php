7e8c2bfdc8b2dd0c882fe1a4f43dfd2b2f356463
JonathanMarandeau <jonathan.marandeau@gmail.com>
2018-05-18 09:22:58 +0200

    Afficher l'index 5 du tableau

---
+++ php5/exercice3/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP Partie 5 Exercice 3</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <h1>PHP Partie 5 Exercice 3</h1>
+      <div class="order">
+        <p>Avec le tableau de l'exercice , afficher la valeur de l'index 5.</p>
+      </div>
+    <h2>Exercice 3</h2>
+      <div class="content">
+        <!--Je crée un tableau mois-->
+        <p><?php $months = array ('janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'décembre'); ?></p>
+        <!--J'affiche la ligne 6 du tableau-->
+        <p><?php echo $months[6]; ?></p>
+      </div>
+  </body>
+</html>
